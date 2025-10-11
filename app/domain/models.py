09ec89888850e2072fff8de2c384c720f27ad9bc
Natalia Araújo <natalia.araujo9918@outlook.com.br>
2025-10-11 00:00:30 -0300

    criado tabela no postgresql com alembic

--- app/domain/models.py
+++ app/domain/models.py
@@ -1,11 +1,15 @@
-from dataclasses import dataclass
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
+Base = declarative_base()
 
-@dataclass(frozen=True)
-class Book:
-    title: str
-    price: str
-    rating: int
-    availability: str
-    category: str
-    image: str
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    price = Column(String, nullable=False)
+    rating = Column(Integer, nullable=False)
+    availability = Column(String, nullable=False)
+    category = Column(String, nullable=False)
+    image = Column(String, nullable=False)
