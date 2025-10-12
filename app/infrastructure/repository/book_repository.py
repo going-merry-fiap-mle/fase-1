8888a197b9cbf7e9a0ce86dbc02dfdb135f0f955
MatheusLucasAndrade <matheus_toty@hotmail.com>
2025-10-12 14:37:21 -0300

    fix: tests

--- app/infrastructure/repository/book_repository.py
+++ app/infrastructure/repository/book_repository.py
@@ -4,14 +4,13 @@ from app.port.book_port import IBookRepository
 
 class BookRepository(IBookRepository):
 
-    def get_books(self) -> list[Book]:      
+    def get_books(self) -> list[Book]:
         # aqui deve ser a query sqlalchemy para a buscar os livros no banco
         test = Book(
             title="fake",
             price="10euro",
             rating=1,
             availability="In stock",
-            category="Poetry",
-            image="fakeimage.jpg",
+            image_url="fakeimage.jpg",
         )
         return [test]
