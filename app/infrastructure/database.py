2aeaf713d07f2b64cf30c1fa6c6b01d37df45e6e
Natalia Gonçalves Araújo <157840748+NataliaAraujo99@users.noreply.github.com>
2025-10-12 14:39:05 -0300

    Feature/create database (#41)
    * criado tabela no postgresql com alembic
    
    * criado conexao com o postgresql local
    
    * resolvido comentarios do code review
    
    * "criado teste unitario e ajustado a URL de conexao do alembic com o banco na AWS"
    
    * correcao dos index na model
    
    * ajuste dockerfile
    
    * perf: alembic on docker
    
    * fix: tests
    
    ---------
    
    Co-authored-by: MatheusLucasAndrade <matheus_toty@hotmail.com>

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -2,3 +2,18 @@
 Módulo de persistência de dados.
 Implemente aqui a lógica para salvar e recuperar dados do sistema.
 """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+DATABASE_URL = "postgresql+psycopg2://postgres:admin@localhost:5432/postgres"  # substituir pela env futuramente
+
+class Database:
+    def __init__(self, db_url: str = DATABASE_URL):
+        self.engine = create_engine(db_url, echo=False)
+        self.SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+
+    def get_session(self) -> Session:
+        return self.SessionLocal()
+
+# Instância global para uso em toda a aplicação
+db = Database()
--- app/tests/api/test_books.py
+++ app/tests/api/test_books.py
@@ -1,12 +1,17 @@
 def test_books_endpoint(client):
-    response = client.get('/api/v1/books')
+    response = client.get("/api/v1/books")
     assert response.status_code == 200 or response.status_code == 501
+
 
 def test_books_search_endpoint(client):
-    response = client.get('/api/v1/books/search')
+    response = client.get("/api/v1/books/search")
     assert response.status_code == 200 or response.status_code == 501
 
-def test_books_id_endpoint(client):
-    response = client.get('/api/v1/books/1')
-    assert response.status_code == 200 or response.status_code == 404 or response.status_code == 501
 
+def test_books_id_endpoint(client):
+    response = client.get("/api/v1/books/1")
+    assert (
+        response.status_code == 200
+        or response.status_code == 404
+        or response.status_code == 501
+    )
