f9a79fe343fb97754bc786336954db9321985000
Pedro Henrique <phperesdasilva@gmail.com>
2025-10-23 19:44:22 -0300

    __init__ updated

--- app/controller/books/__init__.py
+++ app/controller/books/__init__.py
@@ -1,3 +1,4 @@
 from app.controller.books.get_book_controller import GetBookController
+from app.controller.books.get_books_by_price_controller import GetBooksByPriceController
 
-__all__ = ["GetBookController"]
+__all__ = ["GetBookController", "GetBooksByPriceController"]
--- app/usecases/book/__init__.py
+++ app/usecases/book/__init__.py
@@ -1,3 +1,4 @@
 from app.usecases.book.get_book_use_case import GetBookUseCase
+from app.usecases.book.get_books_by_price_use_case import GetBooksByPriceUseCase
 
-__all__ = ["GetBookUseCase"]
+__all__ = ["GetBookUseCase", "GetBooksByPriceUseCase"]
